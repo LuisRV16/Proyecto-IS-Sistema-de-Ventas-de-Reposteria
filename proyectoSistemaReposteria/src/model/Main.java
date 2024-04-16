e05f00365dd528af6f568070cf32cccb3bf35515
AOimortal117 <manoloaodidi@gmail.com>
2024-04-15 20:32:29 -0600

    No sabemos usar vscode

--- proyectoSistemaReposteria/src/model/Main.java
+++ proyectoSistemaReposteria/src/model/Main.java
@@ -1,22 +1,55 @@
 package model;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class Main extends Application{
-    
+public class Main extends Application {
+
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/mainMenu/loginFrame.fxml"));
-        Scene scene = new Scene(root);
-        //stage.getIcons().add(new Image("images/icon.jpg"));
-        stage.setTitle("Login");
-        stage.setScene(scene);
-        stage.setResizable(true);
+    public void start(Stage stage) {
+        int primaryMon;
+        Screen primary = Screen.getPrimary();
+        for (int i = 0; i < Screen.getScreens().size(); i++) {
+            if (Screen.getScreens().get(i).equals(primary)) {
+                primaryMon = i;
+                System.out.println("primary: " + i);
+                break;
+            }
+        }
+        
+        Screen screen2 = Screen.getScreens().get(primaryMon);
+        Stage stage2 = new Stage();
+        stage2.setScene(new Scene(new Label("primary")));
+        // tenemos que establecer la posición de la ventana en el monitor principal:
+        stage2.setX(screen2.getVisualBounds().getMinX());
+        stage2.setY(screen2.getVisualBounds().getMinY());
+        stage2.setFullScreen(true);
+        stage2.show();
+        Label label = new Label("monitor " + i);
+        stage.setScene(new Scene(label));
+        System.out.println(Screen.getScreens().size());
+        Screen screen = Screen.getScreens().get(i); // i es el id del monitor
+
+        // establecer la posición a uno de los "slave" -monitores:
+        stage.setX(screen.getVisualBounds().getMinX());
+        stage.setY(screen.getVisualBounds().getMinY());
+
+        // establecer las dimensiones para el tamaño de la pantalla:
+        stage.setWidth(screen.getVisualBounds().getWidth());
+        stage.setHeight(screen.getVisualBounds().getHeight());
+
+        // muestra el escenario sin decoraciones (barra de título y bordes de ventana):
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
 
