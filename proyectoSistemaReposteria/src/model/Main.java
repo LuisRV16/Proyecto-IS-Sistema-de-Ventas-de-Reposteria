af99468d139fd41fc3b9189d7f438367ae6a6dc6
LuisRV16 <turey0412@gmail.com>
2024-04-15 20:48:19 -0600

    YAAAAAAAAAAAAAAAA

--- proyectoSistemaReposteria/src/model/Main.java
+++ proyectoSistemaReposteria/src/model/Main.java
@@ -27,26 +27,26 @@ public class Main extends Application {
             }
         }
         
-        Screen screen2 = Screen.getScreens().get(primaryMon);
+        //Screen screen2 = Screen.getScreens().get(primaryMon);
         Stage stage2 = new Stage();
         stage2.setScene(new Scene(new Label("primary")));
         // tenemos que establecer la posición de la ventana en el monitor principal:
-        stage2.setX(screen2.getVisualBounds().getMinX());
-        stage2.setY(screen2.getVisualBounds().getMinY());
+        //stage2.setX(screen2.getVisualBounds().getMinX());
+        //stage2.setY(screen2.getVisualBounds().getMinY());
         stage2.setFullScreen(true);
         stage2.show();
-        Label label = new Label("monitor " + i);
-        stage.setScene(new Scene(label));
+        //Label label = new Label("monitor " + i);
+        //stage.setScene(new Scene(label));
         System.out.println(Screen.getScreens().size());
-        Screen screen = Screen.getScreens().get(i); // i es el id del monitor
+        //Screen screen = Screen.getScreens().get(i); // i es el id del monitor
 
         // establecer la posición a uno de los "slave" -monitores:
-        stage.setX(screen.getVisualBounds().getMinX());
-        stage.setY(screen.getVisualBounds().getMinY());
+        //stage.setX(screen.getVisualBounds().getMinX());
+        //stage.setY(screen.getVisualBounds().getMinY());
 
         // establecer las dimensiones para el tamaño de la pantalla:
-        stage.setWidth(screen.getVisualBounds().getWidth());
-        stage.setHeight(screen.getVisualBounds().getHeight());
+        //stage.setWidth(screen.getVisualBounds().getWidth());
+        //stage.setHeight(screen.getVisualBounds().getHeight());
 
         // muestra el escenario sin decoraciones (barra de título y bordes de ventana):
         stage.initStyle(StageStyle.UNDECORATED);
