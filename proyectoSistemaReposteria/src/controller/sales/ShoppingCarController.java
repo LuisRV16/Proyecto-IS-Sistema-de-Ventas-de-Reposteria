e0dfbe37da9d7fcb24245d403c2776dd0157b19d
AOimortal117 <manoloaodidi@gmail.com>
2024-04-17 09:19:00 -0600

    Frames actualizados

--- proyectoSistemaReposteria/src/controller/sales/ShoppingCarController.java
+++ proyectoSistemaReposteria/src/controller/sales/ShoppingCarController.java
@@ -1,20 +1,31 @@
 package controller.sales;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 
 public class ShoppingCarController {
+    Stage stage;
+    @FXML
+    private AnchorPane anchorPane;
 
     @FXML
     private Button btnGoBack;
 
     @FXML
     private Button btnProceedPay;
+
+    @FXML
+    private Button btnFullScreen;
 
     @FXML
     private Label lblFinalPrice;
@@ -41,4 +52,19 @@ public class ShoppingCarController {
 
     }
 
+    @FXML
+    void fullScreen(ActionEvent event){
+        ObservableList<Screen> screens = Screen.getScreens();//Get list of Screens
+        Rectangle2D bounds = screens.get(0).getVisualBounds();
+        iniciar(stage);
+    }
+
+    public void iniciar(Stage stage){
+        stage.setFullScreen(true);
+        this.stage = stage;
+    }
+
+    void fullStage(Stage stage){
+        stage.setFullScreen(true);
+    }
 }
--- proyectoSistemaReposteria/src/model/Main.java
+++ proyectoSistemaReposteria/src/model/Main.java
@@ -1,56 +1,52 @@
 package model;
 
+import java.io.IOException;
+
+import controller.sales.ShoppingCarController;
 import javafx.application.Application;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
-        int primaryMon;
-        Screen primary = Screen.getPrimary();
-        for (int i = 0; i < Screen.getScreens().size(); i++) {
-            if (Screen.getScreens().get(i).equals(primary)) {
-                primaryMon = i;
-                System.out.println("primary: " + i);
-                break;
-            }
-        }
-        
-        //Screen screen2 = Screen.getScreens().get(primaryMon);
-        Stage stage2 = new Stage();
-        stage2.setScene(new Scene(new Label("primary")));
-        // tenemos que establecer la posición de la ventana en el monitor principal:
-        //stage2.setX(screen2.getVisualBounds().getMinX());
-        //stage2.setY(screen2.getVisualBounds().getMinY());
-        stage2.setFullScreen(true);
-        stage2.show();
-        //Label label = new Label("monitor " + i);
-        //stage.setScene(new Scene(label));
-        System.out.println(Screen.getScreens().size());
-        //Screen screen = Screen.getScreens().get(i); // i es el id del monitor
+    public void start(Stage stage) throws IOException {
+        ObservableList<Screen> screenSizes = Screen.getScreens();
+        screenSizes.forEach(screen -> {
+            System.out.println(screen.getBounds());
+        });
 
-        // establecer la posición a uno de los "slave" -monitores:
-        //stage.setX(screen.getVisualBounds().getMinX());
-        //stage.setY(screen.getVisualBounds().getMinY());
-
-        // establecer las dimensiones para el tamaño de la pantalla:
-        //stage.setWidth(screen.getVisualBounds().getWidth());
-        //stage.setHeight(screen.getVisualBounds().getHeight());
-
-        // muestra el escenario sin decoraciones (barra de título y bordes de ventana):
-        stage.initStyle(StageStyle.UNDECORATED);
+        Parent root = FXMLLoader.load(getClass().getResource("/view/product/product.fxml"));
+        Parent root2 = FXMLLoader.load(getClass().getResource("/view/sales/ShoppingCar.fxml"));
+        Scene scene = new Scene(root);
+        Scene scene2 = new Scene(root2);
+        stage.setTitle("1");
+        stage.setScene(scene);
+        stage.setResizable(true);
         stage.show();
+
+        if (screenSizes.size() > 0) {
+            Rectangle2D bounds = screenSizes.get(1).getVisualBounds();
+            
+            Stage secondStage = new Stage();
+            secondStage.setTitle("2");
+            secondStage.setScene(scene2);
+            secondStage.setX(bounds.getMinX());
+            secondStage.setY(bounds.getMinY());
+            secondStage.setMaximized(true);
+            secondStage.setResizable(false);
+            secondStage.show();
+        }
+
     }
 
     public static void main(String[] args) {
