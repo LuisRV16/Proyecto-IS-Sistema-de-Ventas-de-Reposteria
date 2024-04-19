f097b454b79117e7f430738c0e0ca49bf3a08e0f
AOimortal117 <manoloaodidi@gmail.com>
2024-04-18 18:07:46 -0600

    Actualizacion

--- proyectoSistemaReposteria/src/model/Main.java
+++ proyectoSistemaReposteria/src/model/Main.java
@@ -16,26 +16,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-<<<<<<< HEAD
         ObservableList<Screen> screenSizes = Screen.getScreens();
         Stage secondStage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("/view/client/agregaCliente.fxml"));
         Parent root2 = FXMLLoader.load(getClass().getResource("/view/sales/ShopCart.fxml"));
-=======
-        // ObservableList<Screen> screenSizes = Screen.getScreens();
-        // screenSizes.forEach(screen -> {
-        //     System.out.println(screen.getBounds());
-        // System.out.println("???");
-        // });
-
-        Parent root = FXMLLoader.load(getClass().getResource("/view/client/agregaCliente.fxml"));
-        // Parent root2 = FXMLLoader.load(getClass().getResource("/view/sales/ShoppingCart.fxml"));
->>>>>>> 89ab59950248d9d75873cb997a485f038c7882e9
         Scene scene = new Scene(root);
-        // Scene scene2 = new Scene(root2);
+        Scene scene2 = new Scene(root2);
         stage.setTitle("1");
         stage.setScene(scene);
-<<<<<<< HEAD
         secondStage.setTitle("2");
         secondStage.setScene(scene2);
         stage.setResizable(false);
@@ -53,36 +41,12 @@ public class Main extends Application {
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setMaximized(true);
         
-        // secondStage.show();
+        secondStage.show();
         stage.show();
     }
 
     public static void main(String[] args) {
         launch(args);  
-=======
-        stage.setResizable(true);
-        stage.show();
-
-        // if (screenSizes.size() > 1) {
-        //     Rectangle2D bounds = screenSizes.get(1).getVisualBounds();
-            
-        //     Stage secondStage = new Stage();
-        //     secondStage.setTitle("2");
-        //     secondStage.setScene(scene2);
-        //     secondStage.setX(bounds.getMinX());
-        //     secondStage.setY(bounds.getMinY());
-        //     secondStage.setMaximized(true);
-        //     secondStage.setResizable(false);
-        //     secondStage.show();
-        // }
-
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Hol");
-        launch(args);
-        
->>>>>>> 89ab59950248d9d75873cb997a485f038c7882e9
     }
 
 }
