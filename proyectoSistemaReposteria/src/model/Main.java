174c4ef708b656b1c6facda5319d29c58250a635
LuisRV16 <turey0412@gmail.com>
2024-04-21 13:14:47 -0600

    Cambios de Ayer y Hoy

--- proyectoSistemaReposteria/src/model/Main.java
+++ proyectoSistemaReposteria/src/model/Main.java
@@ -2,6 +2,8 @@ package model;
 
 import java.io.IOException;
 
+import controller.client.MenuClienteController;
+import controller.product.ProductController;
 import javafx.application.Application;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
@@ -14,34 +16,80 @@ import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
+    // @Override
+    // public void start(Stage stage) throws IOException {
+    //     ObservableList<Screen> screenSizes = Screen.getScreens();
+    //     Stage secondStage = new Stage();
+    //     Parent root = FXMLLoader.load(getClass().getResource("/view/client/agregaCliente.fxml"));
+    //     Parent root2 = FXMLLoader.load(getClass().getResource("/view/sales/ShopCart.fxml"));
+    //     Scene scene = new Scene(root);
+    //     // Scene scene2 = new Scene(root2);
+    //     stage.setTitle("1");
+    //     stage.setScene(scene);
+    //     secondStage.setTitle("2");
+    //     // secondStage.setScene(scene2);
+    //     stage.setResizable(false);
+    //     if (screenSizes.size() > 1) {
+    //         Rectangle2D bounds = screenSizes.get(1).getVisualBounds();
+    //         secondStage.setX(bounds.getMinX());
+    //         secondStage.setY(bounds.getMinY());
+    //         secondStage.setMaximized(true);
+    //         secondStage.setResizable(false);
+    //         stage.setMaximized(true);
+    //         stage.setResizable(false);
+    //     }else{
+    //         secondStage.setResizable(true);
+    //     }
+    //     stage.initStyle(StageStyle.UNDECORATED);
+    //     stage.setMaximized(true);
+        
+    //     // secondStage.show();
+    //     stage.show();
+    // }
+
+    // @Override
+    // public void start(Stage stage) throws IOException {
+
+    //     SQLConnection con = new SQLConnection();
+    //     con.setUser("sa");
+    //     con.setPassword("sasa");
+
+    //     FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/product/product.fxml"));
+    //     Parent root = loader.load();
+    //     ProductController controller = loader.getController();
+    //     // Manda la conexion a la base de datos
+    //     controller.setCon(con.getConnection());
+    //     controller.inic();
+    //     // Inicializa todos los componentes requeridos
+    //     Scene scene = new Scene(root);
+    //     stage.setTitle("Prueba");
+    //     stage.setScene(scene);
+    //     stage.setResizable(false);
+    //     stage.show();
+    // }
+
+    // public static void main(String[] args) {
+    //     launch(args);  
+    // }
+
     @Override
     public void start(Stage stage) throws IOException {
-        ObservableList<Screen> screenSizes = Screen.getScreens();
-        Stage secondStage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/view/client/agregaCliente.fxml"));
-        Parent root2 = FXMLLoader.load(getClass().getResource("/view/sales/ShopCart.fxml"));
-        Scene scene = new Scene(root);
-        Scene scene2 = new Scene(root2);
-        stage.setTitle("1");
-        stage.setScene(scene);
-        secondStage.setTitle("2");
-        // secondStage.setScene(scene2);
-        stage.setResizable(false);
-        if (screenSizes.size() > 1) {
-            Rectangle2D bounds = screenSizes.get(1).getVisualBounds();
-            secondStage.setX(bounds.getMinX());
-            secondStage.setY(bounds.getMinY());
-            secondStage.setMaximized(true);
-            secondStage.setResizable(false);
-            stage.setMaximized(true);
-            stage.setResizable(false);
-        }else{
-            secondStage.setResizable(true);
-        }
-        stage.initStyle(StageStyle.UNDECORATED);
-        stage.setMaximized(true);
+
+        SQLConnection con = new SQLConnection();
+        con.setUser("sa");
+        con.setPassword("sasa");
         
-        secondStage.show();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/client/menuCliente.fxml"));
+        Parent root = loader.load();
+        MenuClienteController controller = loader.getController();
+        // Manda la conexion a la base de datos
+        controller.setCon(con.getConnection());
+        controller.inic();
+        // Inicializa todos los componentes requeridos
+        Scene scene = new Scene(root);
+        stage.setTitle("Prueba");
+        stage.setScene(scene);
+        stage.setResizable(true);
         stage.show();
     }
 
@@ -50,3 +98,4 @@ public class Main extends Application {
     }
 
 }
+
